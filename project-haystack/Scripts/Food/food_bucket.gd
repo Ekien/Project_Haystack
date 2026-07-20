41a42aedf2f3ae927973a5f3395dc60a30dad14e
Ekien <77694124+Ekien@users.noreply.github.com>
2026-07-20 18:17:30 +0100

    Created the pickable food bucket and can now properly add hay to it.
    TO DO:
    - Add the ability to clear the hay from the bucket.
    - CLEAN EVERYTHING UP. ORGANISATION.

--- project-haystack/Scripts/Food/food_bucket.gd
+++ project-haystack/Scripts/Food/food_bucket.gd
@@ -3,11 +3,14 @@ extends Node3D
 @export var bucket_snap_zone: XRToolsSnapZone
 var bucket_snap_avaliable: bool
 
+# ADD ITEM TO DROP AS A VARIABLE (in this case hay)
+
 var hay_amount: int = 0
 
 @onready var hay_third: Node3D = $Hay_Third
 @onready var hay_half: Node3D = $Hay_Half
 @onready var hay_full: Node3D = $Hay_Full
+
 
 enum hay_states {
 	ONETHIRDFULL,
@@ -32,7 +35,6 @@ func update_bucket() -> void:
 			hay_third.visible = false
 			hay_half.visible = false
 			hay_full.visible = true
-			
 	
 	
 	
@@ -47,15 +49,15 @@ func _process(_delta: float) -> void:
 	pass
 
 
-func _on_bucket_snap_zone_has_picked_up(what: Variant) -> void:
-	# Add to hay if first hay is being placed.
-	if hay_amount == 0:
-		hay_amount = 1
-	elif hay_amount == 1:
-		hay_amount = 2
-	elif hay_amount == 2:
-		hay_amount = 3
-	else:
+func _on_bucket_snap_zone_has_picked_up(_what: Variant) -> void:
+	if not hay_amount < 3:
 		print("NO MORE! IM FULL")
+		return
 		
+	# Add to hay if first hay is being placed.
+	hay_amount += 1;
+	
+	# Delete the hay from the snap zone before updating the bucket 
+	_what.queue_free()
+	
 	update_bucket()
